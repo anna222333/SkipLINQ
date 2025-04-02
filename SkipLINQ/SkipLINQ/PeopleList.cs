347e1fb11696c9f5fd446b582dae995d8a11dd29
anna222333 <anna.sonny48@gmail.com>
2025-04-02 15:06:56 +0300

    Add files via upload

---
+++ SkipLINQ/SkipLINQ/PeopleList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkipLINQ
+{
+    public class PeopleList
+    {
+        public static readonly List<People> people = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Joonas",
+                Age = 33,
+                GenderId = Guid.Parse("438ab9c9-b7a5-4fa7-be53-8bac6f7dd14e")
+            },
+            new People()
+            {
+                Id = 2,
+                Name = "Moona",
+                Age = 21,
+                GenderId = Guid.Parse("c991bde5-dade-4b34-8acc-39381c95a1a7")
+            },
+            new People()
+            {
+                Id = 3,
+                Name = "Ron",
+                Age = 18,
+                GenderId = Guid.Parse("438ab9c9-b7a5-4fa7-be53-8bac6f7dd14e")
+            },
+            new People()
+            {
+                Id = 4,
+                Name = "Jane",
+                Age = 20,
+                GenderId = Guid.Parse("c991bde5-dade-4b34-8acc-39381c95a1a7")
+            },
+            new People()
+            {
+                Id = 5,
+                Name = "Mari",
+                Age = 17,
+                GenderId = Guid.Parse("c991bde5-dade-4b34-8acc-39381c95a1a7")
+            },
+            new People()
+            {
+                Id = 6,
+                Name = "Mari",
+                Age = 19,
+                GenderId = Guid.Parse("c991bde5-dade-4b34-8acc-39381c95a1a7")
+            },
+        };
+    }
+}
---
+++ SkipLINQ/SkipLINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SkipLINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------Skip---------");
+            var skip = PeopleList.people.Skip(4);
+
+            foreach (var x in skip)
+            {
+                Console.WriteLine( x.Age); 
+            }
+
+        }
+    }
+}
+// Skip skipping choosed number of choosed items, for example skip first 4 ages in list and 
+//showing in the input last 2 ages of the list(if it was more items in the list it showing more)
